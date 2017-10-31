dcd0a54ea5b423b1100fba538bf52a7ac8b338b2
fantasmack <cmunoz@hub-camp.mx>
2017-10-30 23:38:51 -0600

    Se agrega carpeta vistas - views

---
+++ vistas/plantilla.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="es-LA">
+	<head>
+		<meta charset="UTF-8">
+		<title>Proyecto Ecommerce</title>
+		<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+		<meta name="description" content="Descripción del sitio">
+		<link rel="icon" href="img/favicon.png">
+		<link rel="stylesheet" href="css/reset.css">
+		<link rel="stylesheet" href="css/main.css">
+	</head>
+	<body>
+		<head>
+			
+		</head>
+		<main>
+			
+		</main>
+		<footer>
+			
+		</footer>
+		<script src="js/main.js"></script>
+	</body>
+</html>
