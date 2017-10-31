423897201fb6333658e28ccaa08a2d5734609f2a
Fantasmack <cmunoz@hub-camp.mx>
2017-10-31 17:04:22 -0600

    Se agrega carpeta modulos

---
+++ vistas/modulos/cabecera.php
@@ -0,0 +1,44 @@
+<div class="fila top flex aling_center">
+	<div class="col-2">
+		<ul class="flex">
+			<li><a href="#">En</a></li>
+			<span>|</span>
+			<li><a href="#">Es</a></li>
+		</ul>
+	</div>
+	<div class="col-8">
+		<p></p>
+	</div>
+	<div class="col-2">
+		<ul class="flex j_end">
+			<li><a href="#">Ingresar</a></li>
+			<span>|</span>
+			<li><a href="#">Registrarse</a></li>
+		</ul>
+	</div>
+</div>
+<div class="fila menu flex align_center">
+	<div class="col-2">
+		<a href="#">
+			<img src="../img/plantilla/logo.svg" alt="tienda">
+		</a>
+	</div>
+	<div class="col-8">
+		<nav class="navegacion">
+			<ul class="flex">
+				<li><a href="#">VISTA</a></li>
+				<li><a href="#">SOLARES</a></li>
+				<li><a href="#">HOMBRE</a></li>
+				<li><a href="#">MUJER</a></li>
+				<li><a href="#">NIÑOS</a></li>
+				<li><a href="#">PROBAR</a></li>
+				<li><a href="#">CONTACTO</a></li>
+			</ul>
+		</nav>
+	</div>
+	<div class="col-2">
+		<ul class="flex">
+			
+		</ul>
+	</div>
+</div>
