de8a4ea2f9d6c7043c7988b07cedc3e384ae15dc
Fantasmack <cmunoz@hub-camp.mx>
2017-10-31 17:05:23 -0600

    Se agrega cabecera a plantilla

--- vistas/plantilla.php
+++ vistas/plantilla.php
@@ -5,20 +5,21 @@
 		<title>Proyecto Ecommerce</title>
 		<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
 		<meta name="description" content="Descripción del sitio">
-		<link rel="icon" href="img/favicon.png">
-		<link rel="stylesheet" href="css/reset.css">
-		<link rel="stylesheet" href="css/main.css">
+		<link rel="icon" href="vistas/img/favicon.png">
+		<link rel="stylesheet" href="vistas/css/reset.css">
+		<link rel="stylesheet" href="vistas/css/main.css">
+		<link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet"> 
 	</head>
 	<body>
-		<head>
-		
-		</head>
+		<header>
+			<?php include "vistas/modulos/cabecera.php"; ?>
+		</header>
 		<main>
-			<h1>Prueba controlador</h1>
+			
 		</main>
 		<footer>
 			
 		</footer>
-		<script src="js/main.js"></script>
+		<script src="vistas/js/main.js"></script>
 	</body>
 </html>
