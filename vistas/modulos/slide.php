5428b8f176546d70d9d37f7db4096b00b5b74b7f
Fantasmack <cmunoz@hub-camp.mx>
2017-11-14 11:58:49 -0600

    actual

--- vistas/modulos/slide.php
+++ vistas/modulos/slide.php
@@ -1,19 +1,51 @@
 <div class="fila" id="slide">
 	<div class="col-10">
 		<ul>
-			<li class="capa"></li>
-			<li>
-				<img width="150" src="vistas/img/plantilla/lentes.jpg" alt="">
-			</li>
-			<li>
-				<div class="descripcion">	
-					<hgroup>
-						<h1>nombre producto</h1>
-						<h2>descripcion producto</h2>
-					</hgroup>
-					<button>Producto</button>
-				</div>
-			</li>
+			<div class="slide1">
+				<!-- Slider 1 -->
+				<li>
+					<img width="150" src="vistas/img/plantilla/lentes.jpg" alt="">
+				</li>
+				
+				<li>
+					<div class="descripcion">
+						<span>Categoria</span>	
+						<hgroup>
+							<h1>Supreme/Black</h1>
+							<h2>
+								Lentes bien padrotes creados  con el mismisimo martillo de Tor,
+								con mica blindada nivel 6 para aguantar pinshis balazos !!...
+								Pium pium !! 
+							</h2>
+						</hgroup>
+						<button>Ir Producto</button>
+					</div>
+				</li>
+			</div>
+			<!-- Slider 2 -->
+			<div class="slide2">			
+				<li>
+					<img width="150" src="vistas/img/plantilla/lentes_2.jpg" alt="">
+				</li>
+				
+				<li>
+					<div class="descripcion">
+						<span>Categoria 2</span>	
+						<hgroup>
+							<h1>Supreme/White</h1>
+							<h2>
+								Lentes bien padrotes creados  con el mismisimo martillo de Tor,
+								con mica blindada nivel 6 para aguantar pinshis balazos !!...
+								Pium pium !! 
+							</h2>
+						</hgroup>
+						<button>Ir Producto</button>
+					</div>
+				</li>
+			</div>
+
+
+
 		</ul>
 	</div>
 	<div class="col-2">
